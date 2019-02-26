638da81321facd8f0b14f7e8cd1ccabd21344b3f
Christopher Croal <ccroal@ntlworld.com>
2019-02-26 09:31:24 +0000

    refactored

--- src/components/Film.js
+++ src/components/Film.js
@@ -4,8 +4,7 @@ class Film extends Component{
   render(){
     return(
       <div className="film">
-        <p>{this.props.title}</p>
-        <a href={this.props.url}>IMDB Page</a>
+        <a href={this.props.url}>{this.props.title}</a>
       </div>
     )
   }
